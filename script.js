a5eea121569ec1086f43b97249888cc68ddad117
Oshiwa <93853682+oshiwa@users.noreply.github.com>
2023-11-14 07:36:23 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 window.addEventListener('load', () => {
-    document.getElementById('closead').addEventListener('click', () => {
-        let adOne = document.querySelector('.ad-space-two');
-        adOne.style.display = 'none';
-    });
     document.querySelector('.mv').addEventListener('click', () => {
         window.location.href = '#';
         alert('Under Development')
     });
 });
+/*
 setTimeout(function() {
     let adOne = document.querySelector('.ad-space-two');
     adOne.style.display = 'block';
 }, 1000);
+document.getElementById('closead').addEventListener('click', () => {
+        let adOne = document.querySelector('.ad-space-two');
+        adOne.style.display = 'none';
+    });
+*/
